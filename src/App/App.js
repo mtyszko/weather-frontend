52a2665dd73fc09e40764801c90f48f5a8a7a47b
mtyszko <michal.tyszko@gmail.com>
2020-08-31 14:10:24 +0200

    setup

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './App.sass';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return <h1>hello misio!</h1>;
+  }
+}
+
+export default App;
